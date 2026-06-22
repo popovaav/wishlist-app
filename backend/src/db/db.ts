eeb36260d951c943e393af15a3ec201b2efb4123
popovaav <popova.av@ukr.net>
2026-06-22 11:35:57 -0300

    refactor: use database url for postgres connection

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -2,12 +2,7 @@ import { Pool } from 'pg';
 import 'dotenv/config';
 
 const pool = new Pool({
-
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT),
-
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
